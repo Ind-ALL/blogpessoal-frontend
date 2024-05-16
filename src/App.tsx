b0a045f9faae60c4d31c8d9d384c494aeac626ba
Ingrid Alves <alves96.ingrid@gmail.com>
2024-05-16 08:36:37 -0300

    Iniciando Front-End do Projeto Blog Pessoal

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Footer from './paginas/footer/footer'
+import Home from './paginas/home/Home'
+
+function App() {
+  return (
+    <>
+      <Home/>
+      <Footer/>
+  
+    </>
+  )
+}
+
+export default App
