d12835cf117caf3b0f79b4de6280a7248b9989a5
Ingrid Alves <alves96.ingrid@gmail.com>
2024-06-08 19:06:38 -0300

    fix: Deploy do Blog Pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
@@ -15,6 +14,7 @@ import Perfil from './pages/perfil/Perfil'
 import { ToastContainer } from 'react-toastify'
 
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from './components/footer/Footer'
 
 function App() {
     return (
